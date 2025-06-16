2642701c6ec06d4ccf6f9d459eb5defa5d77f43f
211G0365 <126606377+211G0365@users.noreply.github.com>
2025-06-15 18:27:56 -0600

    Correccion en Proyectos

--- EncuestaAPI2/DTOs/LoginDTO.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace EncuestaAPI2.DTOs
-{
-    public class LoginDTO
-    {
-        public string Nombre { get; set; } = "";
-        public string Contrasena { get; set; } = "";
-    }
-}
--- EncuestaAPI2/DTOs/UsuarioDTO.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace EncuestaAPI2.DTOs
-{
-    public class UsuarioDTO
-    {
-        public string Nombre { get; set; }
-        public string Contrasena { get; set; }
-        public string Rol { get; set; }
-    }
-}
