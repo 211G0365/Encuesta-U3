d9d71a7381c7c1ac051c0fe6d88ba70f3e1b71e2
briseidagarcia03 <201G0253@rcarbonifera.tecnm.mx>
2025-06-15 21:40:29 -0600

    cambios

---
+++ WebEncuesta/Controllers/EncuestadorController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebEncuesta.Controllers
+{
+    public class EncuestadorController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Aplicar()
+        {
+            return View();
+        }
+
+        public IActionResult Crear()
+        {
+            return View();
+        }
+
+        public IActionResult Editar()
+        {
+            return View();
+        }
+
+        public IActionResult IndexOtras()
+        {
+            return View();
+        }
+
+        public IActionResult IniciarEncuesta()
+        {
+            return View();
+        }
+    }
+}
