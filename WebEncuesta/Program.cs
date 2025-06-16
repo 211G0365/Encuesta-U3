7afca312e0ceca9e7a60ac8e9499f127c1e4f22b
211G0365 <126606377+211G0365@users.noreply.github.com>
2025-06-15 18:39:08 -0600

    se agrego el cors para local

--- WebEncuesta/Program.cs
+++ WebEncuesta/Program.cs
@@ -1,14 +1,31 @@
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(builder =>
+    {
+        builder
+            .WithOrigins("https://localhost:44355")
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    });
+});
+
+// Esto habilita tanto controladores API como MVC:
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
 app.UseStaticFiles();
+
 app.UseRouting();
+
+app.UseCors();
 
 app.UseAuthentication();
 app.UseAuthorization();
+
+app.MapControllers();
 
 app.MapControllerRoute(
     name: "default",
